955cb69afa962370b6ce5b9fb1c29b7764c3b7e7
salemaziel <mymainemail0501@gmail.com>
2021-01-18 01:04:52 -0800

    updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,8 +38,8 @@ module.exports = {
      {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
-        host: 'https://food-demo01.viadelweb.com',
-        sitemap: 'https://food-demo01.viadelweb.com/sitemap.xml',
+        host: 'https://food-demo02.viadelweb.com',
+        sitemap: 'https://food-demo02.viadelweb.com/sitemap.xml',
       }
     },
     {
@@ -68,9 +68,11 @@ module.exports = {
 //    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
     {
       resolve: `gatsby-plugin-sitemap`,
     },
+    {
+      resolve: `gatsby-plugin-offline`,
+    }
   ],
 };
